390347cf76857b4a500f48b39939e87329e13d01
kmtusher97 <kmtusher97@gmail.com>
2025-02-28 22:54:19 +0600

    Add react query to fetch products

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
 import { Box } from '@mui/material';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { ProductTable } from './components';
 
 import './App.css';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Box>
-      <ProductTable />
-    </Box>
+    <QueryClientProvider client={queryClient}>
+      <Box>
+        <ProductTable />
+      </Box>
+    </QueryClientProvider>
   );
 }
 
