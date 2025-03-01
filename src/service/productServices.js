103d14d7e60bb8b1d524dbc4301a9e5dccad1449
nazma98 <nazmasarker1311@gmail.com>
2025-03-01 17:05:13 +0600

    Add search product by name

--- src/service/productServices.js
+++ src/service/productServices.js
@@ -29,10 +29,15 @@ const deleteProduct = async (id) => {
   return true;
 };
 
+const searchProductByName = async (name) => {
+  return await Product.find({ name: name}).select('_id name price image categories');
+}
+
 module.exports = {
   createProduct,
   deleteProduct,
   getAllProducts,
   getProductById,
   updateProduct,
+  searchProductByName,
 };
