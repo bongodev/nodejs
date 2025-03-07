76f3ac88fb0f0be7ddf16f352f6be30728a23e07
kmtusher97 <kmtusher97@gmail.com>
2025-03-07 16:31:30 +0600

    Add pino logger

--- server/src/middleware/index.js
+++ server/src/middleware/index.js
@@ -1,9 +1,7 @@
-const { logRequestMiddleware } = require('./logMiddleware');
 const { errorHandler } = require('./errorHandler');
 const { validatePayload } = require('./validationMiddleware');
 
 module.exports = {
   errorHandler,
-  logRequestMiddleware,
   validatePayload,
 };
--- server/src/middleware/logMiddleware.js
+++
@@ -1,8 +0,0 @@
-const logRequestMiddleware = (req, res, next) => {
-  console.log(`[${req.method}] url: ${req.url}`);
-  next();
-};
-
-module.exports = {
-  logRequestMiddleware,
-};
